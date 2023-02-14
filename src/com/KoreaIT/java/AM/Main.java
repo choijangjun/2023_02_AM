e6d280d49b73ae90afc32f81be036a51873fee0a
choijangjun <jangjun9644@gmail.com>
2023-02-14 11:11:35 +0900

    스캐너로 명령어 입력받기

---
+++ src/com/KoreaIT/java/AM/Main.java
@@ -0,0 +1,25 @@
+package com.KoreaIT.java.AM;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.printf("명령어를 입력하세요.\n");
+		
+		String cmd = sc.nextLine();
+		
+		System.out.printf("입력된 명령어 : %s\n",  cmd);
+		
+		System.out.println("==프로그램 끝==");
+		
+		sc.close();
+	}
+
+}
+
